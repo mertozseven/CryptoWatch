895a9110ff84bd74102e1c9e7d1489a8b18c0303
Mert Adem Özseven <mertozseven@outlook.com>
2024-05-01 22:13:45 +0300

    Initial commit

---
+++ Crypto Watch/Crypto Watch/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto Watch
+//
+//  Created by Mert Ozseven on 1.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
